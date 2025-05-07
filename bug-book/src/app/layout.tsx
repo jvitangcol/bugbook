b6927dc58306046580d27ed0c5ab2ad7307697e2
jefferson <jeffersonvitangcol@gmail.com>
2025-05-07 15:14:10 +0800

    feat: creating feature for post and display the post with user details and  content

--- bug-book/src/app/layout.tsx
+++ bug-book/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -40,6 +41,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
--- bug-book/src/lib/validation.ts
+++ bug-book/src/lib/validation.ts
@@ -11,10 +11,15 @@ export const signUpSchema = z.object({
   password: requiredString.min(8, "Must be at least 8 characters"),
 });
 
+export type SignUpValues = z.infer<typeof signUpSchema>;
+
 export const loginSchema = z.object({
   username: requiredString,
   password: requiredString,
 });
 
-export type SignUpValues = z.infer<typeof signUpSchema>;
 export type LoginValues = z.infer<typeof loginSchema>;
+
+export const createPostSchema = z.object({
+  content: requiredString,
+});
